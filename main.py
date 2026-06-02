d2c40e4ea13f331e0b29abb06dcafca7547caf58
Zakir8585 <psywyhoko@gmail.com>
2026-06-02 22:12:04 +0300

    First commit: bot structure

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+env_path = os.path.join(current_dir, ".env")
+load_dotenv(dotenv_path=env_path)
 
-load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
 
@@ -13,22 +16,40 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+def get_main_keyboard():
+    buttons = [
+        [InlineKeyboardButton(text="🚀 Запустить парсер", callback_data="run_parser")],
+        [
+            InlineKeyboardButton(
+                text="📂 Мой GitHub", url="https://github.com/Zakir8585"
+            )
+        ],
+    ]
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
+
+
 @dp.message(CommandStart())
-async def start_cmd(message: types.Message):
+async def command_start_handler(message: types.Message):
+    user_name = message.from_user.full_name
+
     await message.answer(
-        f"Привет, {message.from_user.first_name}! Это твой первый бот, запущенный на Linux!"
+        text=f"Привет, {user_name}! Я твой первый продвинутый бот. Выбери действие на кнопках ниже:",
+        reply_markup=get_main_keyboard(),
     )
-
-
 @dp.message()
 async def echo_handler(message: types.Message):
     await message.answer(f"Ты написал мне: {message.text}")
 
+@dp.callback_query(lambda callback: callback.data == "run_parser")
+async def process_run_parser(callback: types.CallbackQuery):
+    await callback.answer(text="Парсер запускается...")
+    await callback.message.answer("Имитация парсинга: Сбор данных с сайтов успешно завершен! Собрано 42 товара.")
 
 async def main():
     print("Бот успешно запущен и слушает команды...")
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 x = int(input())
 y = int(input())
+z = int(input())
+if x == 10 and y == 2 and z == 15:
+    for i in range(2):
+        print(x + y**z)
 
-if x > 0 and y > 0:
-    print('1 четверть')
-if x < 0 and y > 0:
-    print('2 четверть')
-if x < 0 and y < 0:
-    print('3 четверть')
-if x > 0 and y < 0:
-    print('4 четверть')
+else:
+    if x == 15 and y == 12 or z == 20:
+        print("Nothing")
+    else:
+        if x == 105 or x == min(0) == max(50) and y == 0 and z == 0:
+            print('get it')
+        
